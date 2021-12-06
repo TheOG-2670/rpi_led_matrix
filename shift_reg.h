3dbf337110b68df4601f21910c4afbda55aead88
TheOG-2670 <ogolan3589@gmail.com>
2021-12-05 19:19:44 -0500

    initial commit

---
+++ shift_reg.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <wiringPi.h>
+#include <wiringShift.h>
+
+#define DATAROW 15
+#define LATCHROW 16
+#define CLOCKROW 1
+
+#define DATACOL 4
+#define LATCHCOL 5
+#define CLOCKCOL 6
+
+#define MRESET 29
+
+void init();
+void writeOut(int rows, int cols);
+//void display(int *pattern, int size, int count, bool loop);
