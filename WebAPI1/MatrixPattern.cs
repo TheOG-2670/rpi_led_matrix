acadbea0745565b3eae0cb33eecc344425629e1a
Omri <omri@email.com>
2021-12-27 19:58:17 -0500

    renamed and updated model

---
+++ WebAPI1/MatrixPattern.cs
@@ -0,0 +1,10 @@
+namespace WebAPI1
+{
+    public class MatrixPattern
+    {
+        public int NumRows { get; set; }
+        public int NumColumns { get; set; }
+
+        public List<List<int>> Pattern { get; set;}
+    }
+}
