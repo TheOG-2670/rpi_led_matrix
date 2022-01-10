54505574b59905329091b88bba656f225ac15f74
TheOG-2670 <ogolan3589@gmail.com>
2022-01-09 20:44:54 -0500

    created 5x5 matrix grid with selectable cells

--- client/App.js
+++ client/App.js
@@ -6,106 +6,74 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, View, StyleSheet, TouchableOpacity} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const initializeMatrix = () => {
+  let matrix = [];
+  for (let i = 0; i < 25; i++) {
+    matrix.push({
+      id: i,
+    });
+  }
+  return matrix;
+};
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [selectedMatrixCells, setSelectedMatrixCells] = useState([]);
+
+  let initialMatrix = initializeMatrix();
+
+  const handlePress = id => {
+    let updatedMatrix = [...selectedMatrixCells];
+    if (selectedMatrixCells.find(i => i.id === id) !== undefined) {
+      updatedMatrix.splice(
+        selectedMatrixCells.indexOf(selectedMatrixCells.find(i => i.id === id)),
+        1,
+      );
+    } else {
+      updatedMatrix.push(initialMatrix.find(i => i.id === id));
+    }
+    setSelectedMatrixCells(updatedMatrix);
+  };
+
+  const render = ({item}) => {
+    const itemStyle = {
+      width: 50,
+      height: 50,
+      margin: 7,
+      backgroundColor: selectedMatrixCells.find(i => i.id === item.id)
+        ? 'green'
+        : 'grey',
+    };
+
+    return (
+      <TouchableOpacity
+        style={itemStyle}
+        onPress={() => handlePress(item.id)}
+      />
+    );
+  };
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.gridContainer}>
+      <FlatList
+        data={initialMatrix}
+        renderItem={render}
+        numColumns={5}
+        extraData={selectedMatrixCells}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  gridContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 150,
+    height: '100%',
   },
 });
 
