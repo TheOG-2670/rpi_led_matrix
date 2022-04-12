10c2447c9d74bd0d1d5d25694d9b003da06fc749
TheOG-2670 <ogolan3589@gmail.com>
2022-04-12 09:48:34 -0400

    added comments and dotenv module; renamed model import

--- node_api/pattern/patternUtils.ts
+++ node_api/pattern/patternUtils.ts
@@ -1,13 +1,21 @@
-import { PatternObjectModel } from "../models/patternModels"
+import { PatternObjectModel } from "./patternModels"
 import * as fs from 'fs'
 import * as path from 'path'
 import {spawn} from 'child_process'
 const file=path.resolve('assets','patterns.txt')
+import * as dotenv from 'dotenv'
 
-
+dotenv.config({"path": "../.env"})
+/*
+    Pattern file copied from 'assets' folder to directory on raspberry pi. 
+    'make' command compiles and execute the program, displaying patterns on the LED matrix,
+    and prints patterns to the console.
+*/
 export const executeRPiPatternDisplay=(): void=>{
+    //@ts-ignore
     spawn('cp', [file, process.env.RPI_DIR])
-    spawn('make', ['-C', process.env.RPI_DIR, 'run', `ARG=${process.env.PATTERN_FILE}`]).stdout.on('data', (data)=>{
+    //@ts-ignore
+    spawn('make', ['-C', process.env.RPI_DIR, 'run', `ARG=${process.env.PATTERN_FILE}`]).stdout.on('data', (data: Uint16Array)=>{
         process.stdout.write(data.toString())
     })
 }
@@ -43,22 +51,30 @@ const constructPattern=(patternObject: string): PatternObjectModel=>{
     return parsedPattern
 }
 
+/**
+ * reads patterns from file, checking that the file is not empty, and 
+ * returns the constructed pattern objects as a response.
+*/
 export const loadPatterns=(): Promise<PatternObjectModel[]> =>{
     return new Promise((resolve, reject)=>{
         let readStream=fs.createReadStream(file, 'utf-8')
         let content: string=""
+
+        //read data in chunks from file and append them as a long string
         readStream.on('data', (data: string)=>{
             content+=data
         })
         .once('end', ()=>{
+            //if the file is empty, send a rejection
             if(content.length===0){
                 reject()
             }
             else{
                 
-                /*separate the pattern strings and parse each one.
-                add the pattern object to an array which will be
-                sent back to add to the response object*/
+                /*  separate the patterns from the original string and parse each one.
+                    add the pattern objects to an array which will be sent back as part 
+                    of the response object.
+                */
                 let patternsArray: PatternObjectModel[] =[]
                 let patternStrings: string[]=content.split('\n\n')
                 patternStrings.forEach((pattern: string)=>{
@@ -72,10 +88,16 @@ export const loadPatterns=(): Promise<PatternObjectModel[]> =>{
     })
 }
 
+/**
+ * each pattern object in the array is parsed and written to file
+ */
 export const savePatterns=(patternObjectArray: PatternObjectModel[]): void=>{
     let writeStream=fs.createWriteStream(file, 'utf-8')
     
-
+    /*
+        each pattern object's row, column, and pattern array are extracted and
+        The object's validity is checked before being written to file.
+    */
     patternObjectArray.forEach((patternObject: PatternObjectModel, i: number)=>{
         const{rows, columns, pattern}=patternObject
         if(rows===0 || columns===0){
