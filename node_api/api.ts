408ff5b45cd84e5278b02f1135a54761fe5bc22a
TheOG-2670 <ogolan3589@gmail.com>
2022-04-12 09:51:28 -0400

    moved swaggerDoc into if condition and replaced port with constant

--- node_api/api.ts
+++ node_api/api.ts
@@ -7,9 +7,10 @@ const HTTP_PORT=8080
 
 //enable swagger for development
 if (process.env.NODE_ENV === 'Development') {
+    const swaggerDoc = require('./swagger.json')
     const swaggerUI = require('swagger-ui-express')
     app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
-    app.listen(5000, () => console.log(`swagger ui listening on 5000`))
+    app.listen(SWAGGER_PORT, () => console.log(`swagger ui listening on ${SWAGGER_PORT}`))
 }
 
 app.use(express.json())
