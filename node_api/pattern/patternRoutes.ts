1c1735b1fa5f888be1496dac070d4b2ec15653eb
TheOG-2670 <ogolan3589@gmail.com>
2023-04-29 10:05:49 -0400

    aded interface types; placed JSON responses in arrays

--- node_api/pattern/patternRoutes.ts
+++ node_api/pattern/patternRoutes.ts
@@ -1,38 +1,38 @@
 import express from 'express'
 import * as utils from './patternUtils'
-import { PatternObjectModel, PatternsResponse } from "./patternModels"
+import { PatternObjectModel, PatternsResponse, ResponseEntity } from "./patternModels"
 const app = express()
 
 
 app.route('/patterns')
-    .get((req: express.Request, res: express.Response<PatternsResponse>) => {
+    .get((req: express.Request, res: express.Response<ResponseEntity>) => {
     utils.loadPatterns().then((content: PatternObjectModel[])=>{
         utils.executeRPiPatternDisplay()
-        res.status(200).json({
+        res.status(200).json([{
             status: "success",
             response: 'display script successfully executed!',
             data: content
-        })
+        }])
     }).catch(()=>{
-        res.status(404).json({
+        res.status(404).json([{
             status: "error",
             response: "no pattern exists"
-        })
+        }])
     })
 })
-.post((req, res) => {
+.post((req: express.Request, res: express.Response<ResponseEntity>) => {
     try {
         utils.savePatterns(req.body)
-        res.status(201).json({
+        res.status(201).json([{
             status: "success",
             response: "patterns saved!"
-        })
+        }])
     } catch (e) {
-	console.log(e)
-        res.status(401).json({
+	    console.log(e)
+        res.status(401).json([{
             status: "error",
             response: "malformed pattern!"
-        })
+        }])
     }
 })
 
